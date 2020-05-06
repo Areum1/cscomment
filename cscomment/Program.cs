fba5cf2f10781f3a530ce90cb448a6c6dc5a2419
Areum1 <s2018s10@e-mirim.hs.kr>
2020-05-06 14:24:13 +0900

    초기 커밋.

---
+++ cscomment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscomment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2, y = 10;
+            //int x = 1, y = 4;
+            //int x = 10, y = 2;
+            //int x = 10, y = 3;
+
+            if (x > 4)
+            {
+                if (y > 2)
+                {
+                    Console.WriteLine(x * y);
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
